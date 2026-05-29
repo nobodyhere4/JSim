98bac87479dd7b03975b48c03b1d2a819a2ec677
Ruthie <s2169121@online.houstonisd.org>
2026-05-29 19:11:52 +0000

    fix: add missing Transform3d import in StateManager

--- vendordep/src/main/java/jsim/api/StateManager.java
+++ vendordep/src/main/java/jsim/api/StateManager.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import jsim.Gamepiece;
 import jsim.Vec3;
